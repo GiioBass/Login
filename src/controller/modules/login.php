491152172d1e013c2ef59b9d78dd8f4399b1d293
Geovanny <williamnavarrocanon@gmail.com>
2020-07-25 15:56:08 -0500

    Update login.php

--- src/controller/modules/login.php
+++ src/controller/modules/login.php
@@ -1,6 +1,32 @@
-<?php 
+<?php
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+require_once "../autoload.php";
 
-echo $username . ' ' . $password;
+session_start();
+
+$username = $_REQUEST["username"];
+$password = $_REQUEST["password"];
+
+//echo json_encode(array('username' => $username, 'password' => $password));
+
+$sql = "SELECT * FROM users WHERE username = '$username' and password='$password'";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+
+	$_SESSION["user"] = $username;
+    echo json_encode(array(
+
+        'status' => true,
+        'url'    => '../src/view/home.html',
+    ));
+} else {
+
+    echo json_encode(array(
+
+        'status' => false,
+    ));
+}
+
+//echo json_encode(array('aceptado' => true));
