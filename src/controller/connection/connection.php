14e00982c9dcb0f22adecc788c791fd768aa3232
Geovanny <williamnavarrocanon@gmail.com>
2020-07-25 15:56:14 -0500

    Update connection.php

--- src/controller/connection/connection.php
+++ src/controller/connection/connection.php
@@ -5,8 +5,8 @@ $db_username = 'geovanny';
 $db_password = '00000';
 $db_name = 'login';
 
-$connection = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
-if (!$connection) {
-	echo 'Failed connection  :v ' . mysqli_errno();
+if (!$conn) {
+	echo json_encode( 'Failed connection  :v ' . mysqli_errno());
 }
